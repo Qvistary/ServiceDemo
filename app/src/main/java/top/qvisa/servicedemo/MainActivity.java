0d62ace574a1296268a83026f9577f3888f53382
qvistary <qvistary@gmail.com>
2019-04-08 18:31:10 +0800

    second Commit

--- app/src/main/java/top/qvisa/servicedemo/MainActivity.java
+++ app/src/main/java/top/qvisa/servicedemo/MainActivity.java
@@ -1,27 +1,55 @@
 package top.qvisa.servicedemo;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mEditText_number1;
+    private EditText mEditText_number2;
+    private static TextView mTextView_result;
+    private String mString_number1, mString_number2;
+    private int number1, number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditText_number1 = findViewById(R.id.et_number1);
+        mEditText_number2 = findViewById(R.id.et_number2);
+        mTextView_result = findViewById(R.id.tv_result);
     }
 
     public void Click_Start_service(View view) {
-        Intent intent = new Intent(this, MyService.class);
-        startService(intent);
+        mString_number1 = mEditText_number1.getText().toString();
+        mString_number2 = mEditText_number2.getText().toString();
+        if (mString_number1.equals("") || mString_number2.equals("")) {
+            mTextView_result.setText("Error! EditText is NULL");
+            mTextView_result.setTextColor(Color.RED);
+            mEditText_number1.setText("");
+        } else {
+            number1 = Integer.parseInt(mString_number1);
+            number2 = Integer.parseInt(mString_number2);
+            Intent intent = new Intent(this, MyService.class);
+            intent.putExtra("a", number1);
+            intent.putExtra("b", number2);
+            startService(intent);
+        }
     }
 
     public void Click_Stop_service(View view) {
         Intent intent = new Intent(this, MyService.class);
         stopService(intent);
+    }
+
+    public static void Update(int result) {
+        mTextView_result.setText("最大的数是 >>>>  " + result);
+        mTextView_result.setTextColor(Color.BLACK);
     }
 
 
--- app/src/main/java/top/qvisa/servicedemo/MyService.java
+++ app/src/main/java/top/qvisa/servicedemo/MyService.java
@@ -6,6 +6,8 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
+    private int a, b, result;
+
     public MyService() {
     }
 
@@ -18,18 +20,28 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("MyService","onCreate");
+        Log.d("MyService", "onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("MyService","onStartCommand");
+        Log.d("MyService", "onStartCommand");
+        a = intent.getIntExtra("a", 0);
+        b = intent.getIntExtra("b", 0);
+        result = Compare(a, b);
+        MainActivity.Update(result);
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("MyService","onDestroy");
+        Log.d("MyService", "onDestroy");
+    }
+
+    public int Compare(int a, int b) {
+        if (a > b) {
+            return a;
+        } else return b;
     }
 }
